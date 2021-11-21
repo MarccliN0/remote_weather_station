3bbef8deb1ee52216ec6a088a69f68751b09e8a7
MarccliN0 <csortos.marcellino@gmail.com>
2021-11-22 01:06:36 +0200

    authentication

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const { MongoClient } = require('mongodb');
 const path = require('path');
+const pbkdf2 = require('pbkdf2');
 
 const app = express();
 
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'pages'))
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('views'))
@@ -20,10 +21,43 @@ async function readDB(CollectionName) {
   return collection;
 }
 
+async function authentication(req, res, next) {
+  let authheader = req.headers.authorization;
+
+  if (!authheader) {
+    res.setHeader('WWW-Authenticate', 'Basic')
+    res.status(401);
+    return next();
+  }
+
+  const salt = 'neverguess';
+
+  let auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pw = pbkdf2.pbkdf2Sync(auth[1], salt, 1, 32, 'sha512').toString('hex');
+  req.user = user;
+
+  const db = await readDB('users');
+  const userMatch = await db.find({username : user}).toArray();
+
+  
+
+ if(!userMatch.length || userMatch[0].password != pw){
+    res.setHeader('WWW-Authenticate', 'Basic')
+    return res.sendStatus(401);
+  }
+  return next()
+}
+
+app.use(authentication)
+
+
 app
 
-.get('/', (req, res) => {
-  res.send('Welcome to the IoT Project');
-})
+  .get('/', (req, res) => {
+    res.render('index.ejs');
+  })
+
+app.listen(3000);
 
 
