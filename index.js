7dd125355a86447df81a3cc1f6b7df007e64d2dd
MarccliN0 <marcell.csortos@metropolia.fi>
2021-11-24 13:06:59 +0200

    no popup authentication

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { MongoClient } = require('mongodb');
 const path = require('path');
 const pbkdf2 = require('pbkdf2');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.set('views', path.join(__dirname, 'pages'))
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.static('pages'))
+app.use(cookieParser());
 
 const url = 'mongodb://localhost:27017';
 const clientdb = new MongoClient(url);
@@ -21,50 +23,58 @@ async function readDB(CollectionName) {
   return collection;
 }
 
+const salt = 'neverguess';
+
 //TODO: implement a better authentication system than browser pop-up
-async function authentication(req, res, next) {
-  let authheader = req.headers.authorization;
+// async function authentication(req, res, next) {
+//   let authheader = req.headers.authorization;
 
-  if (!authheader) {
-    res.status(401);
-    return next();
-  }
-  
+//   if (!authheader) {
+//     res.status(401);
+//     return next();
+//   }
 
-  const salt = 'neverguess';
+//   let auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
+//   const user = auth[0];
+//   const pw = pbkdf2.pbkdf2Sync(auth[1], salt, 1, 32, 'sha512').toString('hex');
+//   req.user = user;
+//   console.log(auth);
 
-  let auth = new Buffer.from(authheader.split(' ')[1], 'base64').toString().split(':');
-  const user = auth[0];
-  const pw = pbkdf2.pbkdf2Sync(auth[1], salt, 1, 32, 'sha512').toString('hex');
-  req.user = user;
-  console.log(auth);
+//   let createAuth = new Buffer.from(auth).toString('base64');
+//   console.log(createAuth)
 
-  let createAuth = new Buffer.from(auth).toString('base64');
-  console.log(createAuth)
+//   const db = await readDB('users');
+//   const userMatch = await db.find({ username: user }).toArray();
 
-  const db = await readDB('users');
-  const userMatch = await db.find({username : user}).toArray();
+//   if (!userMatch.length || userMatch[0].password != pw) {
+//     res.status(401);
+//     return next();
+//   }
+//   return next()
+// }
 
- if(!userMatch.length || userMatch[0].password != pw){
-  res.status(401);
-  return next();
-  }
-  return next()
+async function authentication(req, res, next){
+  let idcookie = req.cookies.idcookie;
+  if(!idcookie){
+    res.redirect('/')
+  }else{
+    next();
+  }  
 }
 
 app
 
-  .use(authentication)
+  
 
   .get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'pages/login.html'));
   })
 
-  .get('/index', (req, res) => {
+  .get('/index', authentication, (req, res) => {
     res.render('index.ejs');
   })
 
-  .post('/postSettings',async (req, res) => {
+  .post('/postSettings', async (req, res) => {
     const data = req.body;
     const db = readDB('userSettings');
     (await db).insertMany([data]);
@@ -85,16 +95,43 @@ app
     res.send(data);
   })
 
-  
+  .post('/login', async (req, res) => {
+    const db = await readDB('users');
+    const username = req.body.username;
+    const userMatch = await db.find({username: username}).toArray();
+    const pw = pbkdf2.pbkdf2Sync(req.body.password, salt, 1, 32, 'sha512').toString('hex');
+    if(userMatch.length && userMatch[0].password == pw){
+      res.cookie("idcookie", `${username}:${pw}`,{ httpOnly: true, expires: new Date(Date.now() + 900000)});
+      res.redirect('/index')
+    }
+  })
+
+  .post('/register', async( req, res) => {
+    const db = await readDB('users');
+    const username = req.body.username;
+    const pw = pbkdf2.pbkdf2Sync(req.body.password, salt, 1, 32, 'sha512').toString('hex');
+
+    const data = {
+      username: username,
+      password: pw
+    }
+    db.insertMany([data]);
+  })
 
 
-  //TODO: implement logging out mechanics
 
-  //check if commit appears in gitlab 
 
-  //TODO: customize with css
+//TODO: implement logging out mechanics
 
-  //TODO: implement light level settings
+//check if commit appears in gitlab 
+
+//TODO: customize with css
+
+//TODO: implement light level settings
+
+app.use((req, res, next) => {
+  res.status(404).send('Page not found')
+}) 
 
 
 app.listen(3000);
--- pages/script/login.js
+++ pages/script/login.js
@@ -1,6 +1,27 @@
 const login = document.querySelector('.login');
-const reg = document.querySelector('.reg');
+const reg = document.querySelector('.register');
 
-login.addEventListener('click', () => {
- 
+login.addEventListener('click', async () => {
+  let username = document.querySelector('.username').value;
+  let password = document.querySelector('.password').value;
+
+  let data = {
+    username,
+    password
+  }
+  console.log(data)
+  await fetch('/login', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)});
+  window.location = '/index'
+})
+
+reg.addEventListener('click', async () => {
+  let username = document.querySelector('.username').value;
+  let password = document.querySelector('.password').value;
+
+  let data = {
+    username,
+    password
+  }
+  console.log(data)
+  await fetch('/register', {method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(data)});
 })
