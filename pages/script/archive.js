d8873e2865ff8a440249fcc8fa74e63c6c9d0c6f
marcelc <marcell.csortos@metropolia.fi>
2021-11-27 16:54:41 +0200

    backtohome button and show graph button for archive page

---
+++ pages/script/archive.js
@@ -0,0 +1,8 @@
+let bTI = document.querySelector('.backToIndex')
+let showhGraph = document.querySelector('.showGraph')
+
+bTI.addEventListener('click', () => {
+  window.location = '/index'
+})
+
+
